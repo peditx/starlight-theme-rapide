ad36c27ed3ecbc966c57f01d3666bdd7f396b10e
PeDitX <pedram.ale@me.com>
2025-09-24 10:27:51 +0330

    Update astro.config.ts

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -5,6 +5,18 @@ import starlightThemeRapide from 'starlight-theme-rapide'
 export default defineConfig({
   integrations: [
     starlight({
+      // ######### START: این قسمت اضافه شده است #########
+      head: [
+        {
+          tag: 'script',
+          // Adds the Netlify Identity widget to all pages
+          attrs: {
+            src: 'https://identity.netlify.com/v1/netlify-identity-widget.js',
+            defer: true,
+          },
+        },
+      ],
+      // ######### END: این قسمت اضافه شده است #########
       credits: true,
       editLink: {
         baseUrl: 'https://github.com/HiDeoo/starlight-theme-rapide/edit/main/docs/',
@@ -31,5 +43,7 @@ export default defineConfig({
       title: 'Starlight Rapide',
     }),
   ],
-  site: 'https://starlight-theme-rapide.vercel.app',
+  // نکته مهم: بهتر است این آدرس را به آدرس سایت خودتان تغییر دهید
+  site: 'https://codes.peditx.ir',
 })
+
